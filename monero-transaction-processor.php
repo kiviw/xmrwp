2ccc2035a81d30e15e1f06dfa14143b16fa0d38d
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-19 14:26:26 +0300

    Create monero-transaction-processor.php

---
+++ monero-transaction-processor.php
@@ -0,0 +1,12 @@
+<?php
+// monero-transaction-processor.php
+
+function generateMoneroSubaddress() {
+    // Code for generating subaddress...
+}
+
+function getLastSubaddress() {
+    // Code for retrieving the last subaddress...
+}
+
+// Other functions related to transaction processing...
