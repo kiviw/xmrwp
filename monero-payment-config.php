4ceb9fb60dc40c4576a028f4127dd2f11e4c648c
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-19 14:25:46 +0300

    Create monero-payment-config.php

---
+++ monero-payment-config.php
@@ -0,0 +1,8 @@
+<?php
+// monero-payment-config.php
+
+define('MONERO_CLI_PATH', '/var/www/monero-x86_64-linux-gnu-v0.18.3.1/monero-wallet-cli');
+define('MONERO_DAEMON_ADDRESS', 'nodex.monerujo.io');
+define('MONERO_WALLET_FILE', '/var/www/monero-x86_64-linux-gnu-v0.18.3.1/mronion');
+define('PASSWORD_FILE_PATH', '/var/www/woo.txt');
+define('SUBADDRESS_FILE_PATH', '/var/www/subaddress.txt');
